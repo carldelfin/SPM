e86b86cfcebb79e736cbdc3b0362dfd68e3d4b16
Carl Delfin <delfin@carldelfin.com>
2017-12-07 09:48:27 +0100

    some minor code cleanup

--- first_level_SPM_designer.m
+++ first_level_SPM_designer.m
@@ -1,59 +1,68 @@
 % ================================================================================
-% =
-% =     FIRST LEVEL ANALYSIS SCRIPT.
-% =     ONSETS ARE IN SEPARATE TEXTFILES FOR EACH SUBJECT.
-% =     RP REGRESSORS ARE IN ONE .MAT FILE FOR EACH SUBJECT.
-% =     SEE ONSET AND RP SCRIPTS ON HOW TO CREATE THESE.
-% =     THIS SCRIPT BUILDS THE FMRI MODEL DESGIN, ESTIMATES THE MODEL,
-% =     AND CREATES TWO CONTRASTS FOR EACH SUBJECT:
-% =     CON_1 IS MOTVATION > NEUTRAL, CON_2 IS NEUTRAL > MOTIVATION
-% =     THE MOTVIATION USED IS NEGATIVE MOTIVATION ONLY, 
-% =     SEE METHODS SECTION IN MANUSCRIPT.
-% =
+% 
+%   This is the FIRST LEVEL ANALYSIS script.
+%
+%   Onsets are in separate textfiles for each subject,
+%   RP regressors are in one .mat file for each subject,
+%   see onset and RP scripts for instructions on how to create these files.
+%
+%   This scripts builds the fMRI model design, estimates the model,
+%   and creates two contrasts for each subject:
+%   CON_1 is motivation > neutral,
+%   CON_2 is neutral > motivation.
+%
+%   NOTE. Motivation used is NEGATIVE motivation only,
+%   see methods section in manuscript.
+% 
 % ================================================================================
+
 %%
 clear all;
 
 spm_jobman('initcfg');
 
-% SPECIFY ONSET TEXTFILES (NO NEED TO CHANGE IF FOLLOWING ONSET SCRIPT)
+% specify onset textfiles (no need to change if following onset script)
 onbase = 'on_base.txt';
 onrew = 'on_rew.txt';
 onneut = 'on_neut.txt';
+
 %%
 % ================================================================================
 
-% SUBJECTS ARE LISTED HERE. SCRIPT NOW RUNS GROUPS SEPARATE FOR REGULAR 
-% AND COUNTERBALANCED, IF NEEDED JUST COMBINE BOTH
+% subjects are listed here, script runs separate groups for regular 
+% and counterbalanced, if needed, combine both
 
-% ARIPIPRAZOLE SUBJECTS:
+% uncomment and rerun script for each group
+
+% ARIPIPRAZOLE:
+%
+% regular: 
+% subjects = {'811' '816' '817' '819' '820' '830' '832' '833'};
+%
+% counterbalanced:
+% subjects = {'839' '840' '844' '847' '849' '855' '856'};
+%
+% HALOPERIDOL:
 %
 % Regular: 
-% subjects={'811' '816' '817' '819' '820' '830' '832' '833'};
+% subjects = {'810' '812' '813' '814' '818' '822' '823' '827'};
 %
-% Counterbalanced:
-% subjects={'839' '840' '844' '847' '849' '855' '856'};
+% counterbalanced:
+% subjects = {'836' '837' '845' '851' '852' '853' '857' '858'};
 %
-% HALDOL SUBJECTS:
+% PLACEBO:
 %
-% Regular: 
-% subjects={'810' '812' '813' '814' '818' '822' '823' '827'};
+% regular: 
+% subjects = {'801' '803' '805' '806' '807' '808' '824' '825' '829' '835'};
 %
-% Counterbalanced:
-% subjects={'836' '837' '845' '851' '852' '853' '857' '858'};
-%
-% PLACEBO SUBJECTS:
-%
-% Regular: 
-% subjects={'801' '803' '805' '806' '807' '808' '824' '825' '829' '835'};
-%
-% Counterbalanced:
-subjects={'835' '838' '842' '843' '846' '848' '850' '854' '859'};
+% counterbalanced:
+% subjects = {'835' '838' '842' '843' '846' '848' '850' '854' '859'};
 %
 % ================================================================================
+
 %%
-% THIS IS THE CURRENT WORKING DIRECTORY, CHANGE ACCORDING TO GROUP:
-cwd = '/Users/delfin/Documents/NORMENT/pharma/scans/placebo/';
+% this is the current working directory, change according to group:
+cwd = '/Users/ ... /pharma/scans/p/';
 
 cd(cwd)
 
@@ -61,31 +70,31 @@ spm('defaults','fMRI')
 
 % ================================================================================
 
-% LOOPING OVER SUBJECTS BEGINS HERE:
+% looping over subjects begins here
 
     for i=1:numel(subjects)
 
         clear matlabbatch
 
 %-----------------------------------------------------------------------
-% Job configuration created by cfg_util (rev $Rev: 4252 $)
+% job configuration created by cfg_util (rev $Rev: 4252 $)
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.stats.fmri_spec.dir = cellstr(fullfile(cwd, subjects{i},'Firstlevel'));
 
-% SPECIFIES UNITS FOR DESIGN AND INTERSCAN INTERVAL:
+% specify units for design and interscan interval:
 matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
 matlabbatch{1}.spm.stats.fmri_spec.timing.RT = 2;
 matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t = 16;
 matlabbatch{1}.spm.stats.fmri_spec.timing.fmri_t0 = 1;
 %%
-% SPECIFIES WHERE TO FIND SCANS AND WHAT THEY ARE NAMED
-% SCRIPT NOW ASSUMES THAT swREW FILES ARE LOCATED IN SEPARATE 'sw' FOLDER:
+% specify where to find scans and what they are named
+% script assumes that swREW files are located in separate 'sw' folder:
 matlabbatch{1}.spm.stats.fmri_spec.sess.scans = cellstr(spm_select('ExtFPList', fullfile(cwd, subjects{i}, 'sw'), '^.*\.nii'));
 %%
-% SPECIFIES ONSETS, DO NOT FORGET TO CHANGE DURATION ACCORDING TO GROUP IF
-% YOU USE THIS SCRIPT FOR OTHER SUBJECTS
+% specify onsets, DO NOT forget to change duration according to group,
+% if this script is used for other subjects
 %%
-% BASELINE ONSET:
+% baseline onset:
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).name = 'Baseline';
 
 fid = fopen(fullfile(cwd, subjects{1}, 'Behavioural', onbase), 'rt');
@@ -98,7 +107,7 @@ matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).duration = 14;
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).tmod = 0;
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(1).pmod = struct('name', {}, 'param', {}, 'poly', {});
 %%
-% REWARD (OR MOTIVATED) ONSET:
+% reward (i.e. motivated) onset:
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).name = 'Reward';
 
 fid = fopen(fullfile(cwd, subjects{1}, 'Behavioural', onrew), 'rt');
@@ -111,7 +120,7 @@ matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).duration = 28;
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).tmod = 0;
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(2).pmod = struct('name', {}, 'param', {}, 'poly', {});
 %%
-% NEUTRAL ONSET:
+% neutral onset:
 matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).name = 'Neutral';
 
 fid = fopen(fullfile(cwd, subjects{1}, 'Behavioural', onneut), 'rt');
@@ -127,7 +136,7 @@ matlabbatch{1}.spm.stats.fmri_spec.sess.cond(3).pmod = struct('name', {}, 'param
 matlabbatch{1}.spm.stats.fmri_spec.sess.multi = {''};
 matlabbatch{1}.spm.stats.fmri_spec.sess.regress = struct('name', {}, 'val', {});
 %%
-% SPECIFIES RP (MULTIPLE REGRESSORS) FILE:
+% specify RP (multiple regressors) file:
 matlabbatch{1}.spm.stats.fmri_spec.sess.multi_reg = {fullfile(cwd, subjects{i}, 'rp', 'multi_reg.mat')};
 %%
 matlabbatch{1}.spm.stats.fmri_spec.sess.hpf = 128;
@@ -138,11 +147,11 @@ matlabbatch{1}.spm.stats.fmri_spec.global = 'None';
 matlabbatch{1}.spm.stats.fmri_spec.mask = {''};
 matlabbatch{1}.spm.stats.fmri_spec.cvi = 'AR(1)';
 %%
-% ESTIMATES THE MODEL, LOCATES SPM.MAT FILE:
+% estimate model, locate SPM.mat file:
 matlabbatch{2}.spm.stats.fmri_est.spmmat = {fullfile(cwd, subjects{i}, 'Firstlevel', 'SPM.mat')};
 matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
 %%
-% CREATES CONTRASTS FOR EACH SUBJECTS, LOCATES THE NOW ESTIMATED SPM.MAT:
+% create contrasts for each subject, locate the now estimated SPM.mat:
 matlabbatch{3}.spm.stats.con.spmmat = {fullfile(cwd, subjects{i}, 'Firstlevel', 'SPM.mat')};
 matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'Rew>Neut';
 matlabbatch{3}.spm.stats.con.consess{1}.tcon.convec = [0 1 -1 0 0 0 0 0 0 0];
@@ -158,9 +167,8 @@ matlabbatch{3}.spm.stats.con.consess{4}.tcon.convec = [0 0 1 0 0 0 0 0 0 0];
 matlabbatch{3}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
 matlabbatch{3}.spm.stats.con.delete = 0;
 
-%% Run batch
+%% run batch
 
 spm_jobman('run',matlabbatch);
 
     end
-    
