544c518035e0119f2ac032044def7b72a242b1c7
Carl Delfin <delfin@carldelfin.com>
2017-12-07 09:35:31 +0100

    some code cleanup

--- RP_textfile_to_matfile.m
+++ RP_textfile_to_matfile.m
@@ -1,18 +1,19 @@
 clear all;
 
-% SUBJECTS NUMBERS GO HERE (WITHOUT _CB)
-subjects={'811' '816' '817' '819' '820' '830' '832' '833' '839' '840' '844' '847' '849' '855' '856'};
+% subject numbers go here (without _cb)
+subjects = {'811' '816' '817' '819' '820' '830' '832' '833' '839' '840' '844' '847' '849' '855' '856'};
 
 formatSpec = ('%f %f %f %f %f %f');
 
-% CURRENT WORKING DIRECTORY > SPECIFIES WER SUBJECTS' FOLDERS ARE
-cwd = '/Users/delfin/Documents/Research/NORMENT/Pharma_Ny/Ariprip/'; % Main directory
+% current working directory, point to where subject folders are
+% this should be set to pharma group folder, change as needed
+cwd = '/Volumes/2TB/norment/pharma/scans/a';
 
-for i=1:numel(subjects);
+for i = 1:numel(subjects);
 
-% VARIABLE NAME OF RP TEXTFILE
+% variable name of RP textfile
 rp = ['rp_Rew' subjects{i} '#001.txt'];
-    
+
 cd(cwd)
 savespec = [cwd, subjects{i}, '/rp'];
 fid = fopen(fullfile(cwd, subjects{i}, 'rp', rp), 'rt');
@@ -22,6 +23,6 @@ fclose(fid);
 R = [rpp{1,1}, rpp{1,2}, rpp{1,3}, rpp{1,4}, rpp{1,5}, rpp{1,6}];
 
 cd(savespec);
-save('multi_reg.mat', 'R'); % THIS IS OUTPUT FOR USE IN FIRST LEVEL ANALYSIS 
+save('multi_reg.mat', 'R'); % this file is for use in first level analysis 
 
 end;
